79255985e682a8104251c008c5b43ee6d9b53bf1
kv8gh <vyaskaran1409@gmail.com>
2025-05-22 15:01:17 +0530

    Heading, Text Color and Highlight color feature is added

--- src/app/document/[documentId]/editor.tsx
+++ src/app/document/[documentId]/editor.tsx
@@ -2,6 +2,8 @@
 
 import TaskList from '@tiptap/extension-task-list'
 import TaskItem from '@tiptap/extension-task-item'
+import Highlight from '@tiptap/extension-highlight'
+import { Color } from '@tiptap/extension-color'
 import Image from '@tiptap/extension-image'
 import FontFamily from '@tiptap/extension-font-family'
 import Underline from '@tiptap/extension-underline'
@@ -52,6 +54,10 @@ export const Editor = ()=>{
         },
         extensions: [
             StarterKit,
+            Color,
+            Highlight.configure({
+                multicolor: true,
+            }),
             FontFamily,
             TextStyle,
             Underline,
--- src/app/document/[documentId]/toolbar.tsx
+++ src/app/document/[documentId]/toolbar.tsx
@@ -5,7 +5,123 @@ import { DropdownMenu,DropdownMenuContent,DropdownMenuTrigger } from "@/componen
 import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
 import { useEditorStore } from "@/store/use-editor-store";
-import { BoldIcon, ChevronDownIcon, ItalicIcon, ListTodoIcon, LucideIcon, MessageSquarePlusIcon, PrinterIcon, Redo2Icon, RemoveFormattingIcon, SpellCheckIcon, UnderlineIcon, Undo2Icon } from "lucide-react";
+import { BoldIcon, ChevronDownIcon, HighlighterIcon, ItalicIcon, ListTodoIcon, LucideIcon, MessageSquarePlusIcon, PrinterIcon, Redo2Icon, RemoveFormattingIcon, SpellCheckIcon, UnderlineIcon, Undo2Icon } from "lucide-react";
+import { type Level } from '@tiptap/extension-heading'
+import { type ColorResult, SketchPicker } from "react-color";
+
+const HighlightColorButton= ()=>{
+    const { editor } = useEditorStore();
+    const value = editor?.getAttributes("highlight").color || "#FFFFFF";
+
+    const onChange = (color:ColorResult)=>{
+        editor?.chain().focus().setHighlight({color: color.hex}).run()
+    };
+
+    return(
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button
+                className="h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+                >
+                    <HighlighterIcon className="size-4"/>
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="p-0">
+                <SketchPicker
+                    color={value}
+                    onChange={onChange}
+                />
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
+
+const TextColorButton= ()=>{
+    const { editor } = useEditorStore();
+    const value = editor?.getAttributes("textStyle").color || "#000000";
+
+    const onChange = (color:ColorResult)=>{
+        editor?.chain().focus().setColor(color.hex).run()
+    };
+
+    return(
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button
+                className="h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+                >
+                    <span className="text-xs">A</span>
+                    <div className="h-0.5 w-full" style={{ backgroundColor:value}}/>
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="p-0">
+                <SketchPicker
+                    color={value}
+                    onChange={onChange}
+                />
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
+
+const HeadingLevelButton = () =>{
+    const {editor} = useEditorStore();
+
+    const headings = [
+        { label: "Normal text", value:0, fontSize: '16px'},
+        { label: "Heading 1", value:1, fontSize: '32px'},
+        { label: "Heading 2", value:2, fontSize: '24px'},
+        { label: "Heading 3", value:3, fontSize: '20px'},
+        { label: "Heading 4", value:4, fontSize: '18px'},
+        { label: "Heading 5", value:5, fontSize: '16px'},
+    ];
+
+    const getCurrentHeading = ()=>{
+        for(let level=1;level<=5; level++){
+            if(editor?.isActive("heading",{level})){
+                return `Heading ${level}`;
+            }
+        }
+
+        return "Normal text";
+    }
+
+    return(
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button className=
+                    "h-7 min-w-7 shrink-0 flex items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+                >
+                    <span className="truncate">
+                        {getCurrentHeading()}
+                    </span>
+                    <ChevronDownIcon className="ml-2 size-4 shrink-0"/>
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+                {headings.map(({label,value,fontSize})=>(
+                    <button
+                        onClick={()=>{
+                            if(value===0){
+                                editor?.chain().focus().setParagraph().run();
+                            }else{
+                                editor?.chain().focus().toggleHeading({level:value as Level}).run();
+                            }
+                        }}
+                        key = {value}
+                        style={{fontSize}}
+                        className={cn(
+                                "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+                                (value === 0 && !editor?.isActive("heading")) || editor?.isActive("heading",{ level: value }) && "bg-neutral-200/80"
+                            )}
+                    >
+                        {label}
+                    </button>
+                ))}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
 
 const FontFamilyButton = ()=>{
     const {editor} = useEditorStore();
@@ -160,10 +276,11 @@ export const Toolbar = ()=>{
                 ))
             }
             <Separator orientation= "vertical" className= "h-6 bg-neutral-300"/>
-            {/* TODO: Font Family */}
-            <FontFamilyButton/>
+                {/* Font Family */}
+                <FontFamilyButton/>
             <Separator orientation= "vertical" className= "h-6 bg-neutral-300"/>
-            {/* TODO: Heading */}
+                {/* Heading */}
+                <HeadingLevelButton/>
             <Separator orientation= "vertical" className= "h-6 bg-neutral-300"/>
             {/* TODO: Font Size */}
             <Separator orientation= "vertical" className= "h-6 bg-neutral-300"/>
@@ -172,8 +289,10 @@ export const Toolbar = ()=>{
                     <ToolbarButton key={item.label} {...item}/>
                 ))
             }
-            {/* TODO: Text color */}
-            {/* TODO: Highlight color */}
+                {/* Text color */}
+                <TextColorButton/>
+                {/* Highlight color */}
+                <HighlightColorButton/>
             <Separator orientation= "vertical" className= "h-6 bg-neutral-300"/>
             {/* TODO: Link */}
             {/* TODO: Image */}
