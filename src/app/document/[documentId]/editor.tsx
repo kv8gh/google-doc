73c68349c4c0c9df7494960b2bc927261fac4dae
kv8gh <vyaskaran1409@gmail.com>
2025-05-24 18:30:08 +0530

    ruler is added

--- src/app/document/[documentId]/editor.tsx
+++ src/app/document/[documentId]/editor.tsx
@@ -21,11 +21,13 @@ import { useEditorStore } from '@/store/use-editor-store';
 import TextStyle from '@tiptap/extension-text-style';
 import { FontSizeExtension } from '@/app/extensions/font-size';
 import { LineHeightExtension } from '@/app/extensions/line-height';
+import { Ruler } from './ruler';
 
 export const Editor = ()=>{
     const { setEditor } = useEditorStore();
 
     const editor = useEditor({
+        immediatelyRender:false,
         onCreate({ editor }){
             setEditor(editor);
         },
@@ -109,6 +111,7 @@ export const Editor = ()=>{
 
     return (
         <div className='size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible'>
+            <Ruler/>
             <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
 
             <EditorContent editor={editor}/>
