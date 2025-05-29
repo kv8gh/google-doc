1f36a9d06a6a565c7c9310aa8af5d6f4efd48bf2
kv8gh <vyaskaran1409@gmail.com>
2025-05-29 14:59:59 +0530

    inbox

--- src/app/document/[documentId]/actions.ts
+++ src/app/document/[documentId]/actions.ts
@@ -1,11 +1,20 @@
 "use server";
 
 import {auth, clerkClient} from "@clerk/nextjs/server";
+import { ConvexHttpClient } from "convex/browser";
+import { Id } from "../../../../convex/_generated/dataModel";
+import { api } from "../../../../convex/_generated/api";
 
 interface orgType {
     id: string,
     role: string,
     slg: string,
+}
+
+const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export async function getDocuments(ids:Id<"documents">[]){
+    return await convex.query(api.documents.getByIds,{ids});
 }
 
 export async function getUsers(){
